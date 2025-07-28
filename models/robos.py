c01805e90062f120b10a13905c47fde03ef60446
Pedro <xelespedro@gmail.com>
2025-07-28 09:50:47 -0300

    ds

--- models/robos.py
+++ models/robos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, LargeBinary, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, Text, LargeBinary, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
